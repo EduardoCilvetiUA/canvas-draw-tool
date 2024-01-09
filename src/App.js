a2515e220a3a5ebbd8d67cf9182c9196b8ce5785
Eduardo Cilveti <eacilveti@miuandes.cl>
2024-01-09 20:11:02 -0300

    2

--- src/App.js
+++ src/App.js
@@ -44,14 +44,51 @@ function App() {
     canvasRef.current.undo();
   }
 
+  const downloadImageWithWhiteBackground = () => {
+    const canvas = document.createElement('canvas');
+    const context = canvas.getContext('2d');
+
+    const image = new Image();
+    image.src = imageDataUrl;
+    image.onload = () => {
+      canvas.width = image.width;
+      canvas.height = image.height;
+      context.fillStyle = '#fff'; // color de fondo
+      context.fillRect(0, 0, canvas.width, canvas.height);
+      context.drawImage(image, 0, 0);
+
+      const whiteBackgroundImageUrl = canvas.toDataURL('image/jpeg');
+      const link = document.createElement('a');
+      link.href = whiteBackgroundImageUrl;
+      link.download = 'image.jpeg';
+      link.click();
+    };
+  };
+
   return (
     <div className="App">
-      <h1>React Canvas Draw</h1>
-      <div style={{ display: 'flex', justifyContent: 'space-between', height: '100vh', margin: '20px' }}>
-        <div>
-          {imageDataUrl && <img src={imageDataUrl} alt="Canvas" />}
+      <h1>Herramienta dibujo en react</h1>
+      <div className ="elementos-dibujos">
+        <div className="image-container" style={{ marginRight: '50px', display: 'flex', flexDirection: 'column' }}>
+          {imageDataUrl && <img src={imageDataUrl} alt="canvas" />}
+          {imageDataUrl &&
+            <Button variant='primary' onClick={downloadImageWithWhiteBackground} style={{ marginTop: '10px' }}>Descargar imagen</Button>
+          }
         </div>
-        <div style={{ marginRight: '20px', display: 'flex', flexDirection: 'column' }}>
+        <div>
+          <CanvasDraw
+            ref={canvasRef}
+            brushRadius={brushRadius}
+            brushColor={brushColor}
+            catenaryColor="black"
+            hideGrid={true}
+            hideInterface={true}
+            lazyRadius={0}
+            style={{ border: '3px solid #000', borderRadius: '10px' }}
+          />
+          <br />
+        </div>
+        <div style={{ marginLeft: '50px', display: 'flex', flexDirection: 'column' }}>
           <Button variant="primary" onClick={handleClearCanvas} style={{ marginTop: '10px', width: '40px', height: '40px' }}>
             <i className="bi bi-eraser-fill"></i>
           </Button>
@@ -85,19 +122,7 @@ function App() {
           </Button>
           <br />
         </div>
-        <div>
-          <CanvasDraw
-            ref={canvasRef}
-            brushRadius={brushRadius}
-            brushColor={brushColor}
-            catenaryColor="black"
-            hideGrid={true}
-            hideInterface={true}
-            lazyRadius={0}
-            style={{ border: '1px solid #000' }}
-          />
-          <br />
-        </div>
+
       </div>
     </div>
   );
